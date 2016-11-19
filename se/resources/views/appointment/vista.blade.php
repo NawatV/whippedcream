ca8e9926144ee9d1e964f67382a76c1cd8c5b2f1
PawasPong <akthanat.p@student.chula.ac.th>
2016-11-19 23:43:07 +0700

    เชรี่ย  PDF ภาษาไทย ได้แล้ว

--- se/resources/views/appointment/vista.blade.php
+++ se/resources/views/appointment/vista.blade.php
@@ -1,17 +1,20 @@
 <head>
-<meta http-equiv="Content-Type" content="charset=window-874" />
-  <style type="text/css">
-    * {
-      font-family: 'Browallia New', sans-serif;
-    }
-    body{
-        font-family: 'Browallia New', sans-serif;
-    }
-  </style>
-  </head>
-  <body>
-  สวัสดี
-  </body>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <style>
+        body {
+            font-family: 'THSarabun';
+            /*font-family: "";*/
+            background-color: black;
+            color: white;
+        }
+    </style>
+
+</head>
+<body>
+สวัสดี ฉันชื่อบุษบา โย่วๆ เพลงๆ  เชรี่ย มึงออกเหอะ
+<br>
+Hello Quick Brown Fox jumped over the lazy dog
+</body>
 
 
 
